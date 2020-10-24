2274824384e5767dc943d3b4d3659695a5e30221
EC2 Default User <ec2-user@ip-172-31-9-86.us-west-1.compute.internal>
2020-10-24 23:23:28 +0000

    make html work

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Marianna Petrovich CST336 CSUMB Lab 1</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        
-        <header>
-            <h1>Marianna Petrovich</h1>
-        </header>
-       <h1>Welcome to CST336 CSUMB Lab 1 Website!</h1>
-    </body>
-</html>
+<?php include_once("index.html"); ?>
